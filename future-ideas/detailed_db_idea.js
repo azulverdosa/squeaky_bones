f8f4c4c94813f222a8b742a5dbf479e91d2e15ac
Ava <atorre@tradewind.aero>
2023-10-02 10:30:39 -0400

    change file type

---
+++ future-ideas/detailed_db_idea.js
@@ -0,0 +1,39 @@
+const detailedDog = {
+  'member id': 12332456532644,
+  'name': 'Gasbode',
+  'breed': 'Schnauzer',
+  'color': 'white & grey',
+  'age': 3,
+  'weight': 12,
+  'photo': '/assets/pet-images/chris-arthur-collins-wosoGykZdBg-unsplash.jpg',
+  'commands': 'sit, stay, here, come',
+  'health concerns': 'arthritis',
+  'medication': {
+    name: 'Metacam',
+    form: 'liquid',
+    strength: 'for dogs',
+    dose: '12kg',
+    frequency: 'SID',
+  },
+  'microchip': { complete: true, number: 1234567567890 },
+  'vaccine status': {
+    rabies: { complete: true, expires: 'date' },
+    dhpp: { complete: false, expires: 'n/a' },
+    bordatella: { complete: true, expires: 'date' },
+  },
+  'flea & tick protection': {
+    'status': false,
+    'brand': 'n/a',
+    'frequency': 'monthly',
+    'next dose due': 'date',
+  },
+  'food allergies': {
+    status: true,
+    specifics: ['chicken', 'beef'],
+  },
+  'photo': {
+    status: true,
+    picture: 'file.png',
+  },
+  'owner comments': 'ear shy',
+};
